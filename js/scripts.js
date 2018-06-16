e39565e8b40fc6a2d8f2b764ec88f47eaefc3a6b
Witold Olszyk <witoldolszyk@gmail.com>
2018-06-16 16:42:35 +0200

    clock

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+function showTime() {
+  var date = new Date();
+  var h = date.getHours();
+  var m = date.getMinutes();
+  var s = date.getSeconds();
+
+
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  s = (s < 10) ? "0" + s : s;
+
+  var time = h + ":" + m + ":" + s;
+  document.getElementById("Clock").innerText = time;
+  document.getElementById("Clock").textContent = time;
+
+  setTimeout(showTime, 1000);
+}
+showTime();
